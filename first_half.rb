95727c98739616498d88d384747534b677c34d40
yasuyukisato <godasper@gmail.com>
2022-03-26 01:53:17 +0900

    問題：文字列no pain no gainに出てくる単語の個数を数えて、ハッシュで表現する

--- first_half.rb
+++ first_half.rb
@@ -8,3 +8,13 @@ arr.each do |m|
     end
 end
 p count
+
+
+# 問題：文字列no pain no gainに出てくる単語の個数を数えて、ハッシュで表現する
+texts = 'no pain no gain'
+def count_string(texts)
+  hash = Hash.new(0)
+  string.split(' ').each { |word| hash[word] += 1}
+  puts hash
+end
+count_string(texts)
